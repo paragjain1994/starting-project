72146526975fbc00eeb8cc69bafe0cb21f10445d
parag <paragjain528@gmail.com>
2022-11-07 17:07:02 +0530

    components with basic functionality added

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,14 @@
+// invoked from CourseInput.js comp
+import React from "react";
+
+import "./Button.css";
+
+const Button = (props) => {
+  return (
+    <button type={props.type} className= 'button' onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
